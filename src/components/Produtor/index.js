0090087b4df7e2b62338ddd65cb8986400e3dddf
Gustavo Souza <gustavogss.dev@gmail.com>
2023-02-20 17:07:26 -0300

    Estilizando lista de produtores

--- src/components/Produtor/index.js
+++ src/components/Produtor/index.js
@@ -1,14 +1,16 @@
 import {View, Text, Image} from 'react-native';
 import React from 'react';
+import styles from './styles';
 
-export default function Produtor({nome, imagem, distancia, estrelas}) {
+export default function Produtor({nome, imagem, distance, estrelas}) {
   return (
-    <View>
-      <Image source={imagem} accessibilityLabel={nome} />
-      <View>
-        <Text>{nome}</Text>
-        <Text>{distancia}</Text>
+    <View style={styles.card}>
+      <Image style={styles.image} source={imagem} accessibilityLabel={nome} />
+      <View style={styles.info}>
+        <Text style={styles.nome}>{nome}</Text>
+        <Text style={styles.distance}>{distance}</Text>
       </View>
+      <Text>{estrelas}</Text>
     </View>
   );
 }
